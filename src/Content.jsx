20a2ecd7d3ba8ba659129106ac250a0eea869942
chash-code <panchalchash@gmail.com>
2025-12-16 23:31:57 +0530

    Add Content component with main content layout

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,1 +1,14 @@
+import React from 'react';
 
+function Content() {
+  return (
+    <main style={{ 
+      textAlign: 'center',
+      padding: '40px'
+    }}>
+      This is Main Content
+    </main>
+  );
+}
+
+export default Content;
