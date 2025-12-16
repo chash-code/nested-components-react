804c092a1796d8e8176cfdda40d809b26ac6c1bd
chash-code <panchalchash@gmail.com>
2025-12-16 23:32:51 +0530

    Add Footer component with styling

---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ 
+      fontSize: '12px',
+      color: '#666',
+      padding: '15px',
+      textAlign: 'center'
+    }}>
+      This is Footer
+    </footer>
+  );
+}
+
+export default Footer;
