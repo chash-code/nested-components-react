c1cc8581d1474d763289e843021a01fe1a39bb4a
chash-code <panchalchash@gmail.com>
2025-12-16 23:30:36 +0530

    Add Navbar component with styling

---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav style={{ 
+      backgroundColor: '#4a90e2', 
+      padding: '20px',
+      color: 'white'
+    }}>
+      This is Navbar
+    </nav>
+  );
+}
+
+export default Navbar;
